12226d1ab241d3c3027c309c1d7c1e0bfb7afad9
Aditya Deshmukh <156204127+iamready07@users.noreply.github.com>
2026-03-15 16:42:52 +0530

    Create index.js
    Signed-off-by: Aditya Deshmukh <156204127+iamready07@users.noreply.github.com>

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily:'Arial', padding:40}}>
+      <h1>CareerSpring Job Portal</h1>
+      <p>From Learning to Earning</p>
+    </div>
+  );
+}
